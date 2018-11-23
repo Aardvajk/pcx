013c8481650384b4e738950fb969ef643cd97093
Paul <aardvajk@live.co.uk>
2018-11-23 15:21:53 +0000

    pcx::optional no longer holds default constructed instance when empty

--- optional.cpp
+++ optional.cpp
@@ -1,3 +1,4 @@
 #include "pcx/optional.h"
 
 const pcx::nullopt_t pcx::nullopt(0);
+const pcx::in_place_t pcx::in_place(0);
--- optional.h
+++ optional.h
@@ -1,7 +1,10 @@
 #ifndef PCX_OPTIONAL_H
 #define PCX_OPTIONAL_H
 
+#include <type_traits>
 #include <utility>
+
+#include <iostream>
 
 namespace pcx
 {
@@ -13,29 +16,40 @@ struct nullopt_t
 
 extern const nullopt_t nullopt;
 
+struct in_place_t
+{
+    constexpr in_place_t(int){ }
+};
+
+extern const in_place_t in_place;
+
 template<typename T> class optional
 {
 public:
-    optional() : t(T()), v(false) { }
-    optional(T t) : t(std::move(t)), v(true) { }
+    optional() : v(false) { }
     optional(nullopt_t) : v(false) { }
+    optional(const T &t) : v(true) { new(&s) T(t); }
+    optional(const optional &o) : v(o.v) { if(v) new(&s) T(*o); }
+    template<typename... Args> optional(in_place_t, Args&&... args) : v(true) { new(&s) T(std::forward<Args>(args)...); }
+    ~optional(){ if(v) reinterpret_cast<T*>(&s)->~T(); }
 
-    optional &operator=(T c){ t = std::move(c); v = true; return *this; }
+    optional &operator=(const optional &o){ if(v) reinterpret_cast<T*>(&s)->~T(); v = o.v; if(v) new(&s) T(*o); return *this; }
 
-    bool operator==(const optional &o) const { return v ? o.v && t == o.t : !o.v; }
+    bool operator==(const optional &o) const { return v ? o.v && *(reinterpret_cast<const T*>(&s)) == *(reinterpret_cast<const T*>(&o.s)) : !o.v; }
     bool operator!=(const optional &o) const { return !(*this == o); }
 
     operator bool() const { return v; }
+    bool valid() const { return v; }
 
-    T &operator*(){ return t; }
-    const T &operator*() const { return t; }
+    T &operator*(){ return *reinterpret_cast<T*>(&s); }
+    const T &operator*() const { return *reinterpret_cast<const T*>(&s); }
 
-    T *operator->(){ return &t; }
-    const T *operator->() const { return &t; }
+    T *operator->(){ return reinterpret_cast<T*>(&s); }
+    const T *operator->() const { return reinterpret_cast<const T*>(&s); }
 
 private:
-    T t;
     bool v;
+    typename std::aligned_storage<sizeof(T)>::type s;
 };
 
 }
